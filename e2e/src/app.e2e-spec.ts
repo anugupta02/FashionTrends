d3f963da9e73bfc1163664b32898508bf59122a5
anu1996 <30476715+anu1996@users.noreply.github.com>
2020-09-05 20:29:05 +0530

    new added functionality

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ng-ecomerce-shop app is running!');
+    expect(page.getTitleText()).toEqual('fashion-trends app is running!');
   });
 
   afterEach(async () => {
